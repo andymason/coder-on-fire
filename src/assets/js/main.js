7a0fb6a6415dbdd7d66e823821ff70dbee309723
Andrew Mason <andrew.mason@guardian.co.uk>
2013-07-23 13:27:23 +0100

    Added missing files

---
+++ src/assets/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: andrew
+ * Date: 19/07/13
+ * Time: 16:39
+ * To change this template use File | Settings | File Templates.
+ */
+define('main', ['hljs', 'test'], function(hljs) {
+    'use strict';
+    hljs.initHighlighting();
+    console.log('in here');
+});
