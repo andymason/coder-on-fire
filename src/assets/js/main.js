3916997f009bda61db3b5fc7d78035dc93a64247
Andrew Mason <a.w.mason@gmail.com>
2013-08-06 23:26:16 +0100

    Fonts

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -12,7 +12,7 @@ define('main', ['hljs', 'backgroundGlitch', 'webfont'], function(hljs) {
     // Prevent font flash by loading afterwards.
     WebFont.load({
         google: {
-            families: ['PT Sans']
+            families: ['PT Sans', 'Merriweather']
         }
     });
 });
