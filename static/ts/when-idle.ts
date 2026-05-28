467c9422f7e77908dba563de5871b2e1bfb1e459
Andrew Mason <andrew@coderonfire.com>
2026-05-28 12:56:05 +0100

    Fix type in whenIdle TS helper.

--- static/ts/when-idle.ts
+++ static/ts/when-idle.ts
@@ -1,9 +1,9 @@
-export const whenIdle = (callback: () => void, timoutDelay = 200): void => {
+export const whenIdle = (callback: () => void, timeoutDelay = 200): void => {
   if (window.requestIdleCallback) {
-    window.requestIdleCallback(callback, { timeout: timoutDelay });
+    window.requestIdleCallback(callback, { timeout: timeoutDelay });
 
     return;
   }
 
-  setTimeout(callback, timoutDelay);
+  setTimeout(callback, timeoutDelay);
 };
